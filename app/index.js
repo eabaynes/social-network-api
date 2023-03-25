d7ac71323a23bb2ed823951fc05676b84ab528b6
Eli Baynes <sbaynes1994@gmail.com>
2023-03-25 01:19:13 -0500

    fix conn problem

--- app/index.js
+++ app/index.js
@@ -1,6 +1,6 @@
 // initialize the server here and connect to the database
 import initServer from './server.js';
-import initDBConn from './config/conn.js';
+import initDBConn from './conn.js';
 
 // initialize the database connection
 initDBConn();
