234a85ec5aca74490299be1a384daf3bb86ed6ff
Eli Baynes <sbaynes1994@gmail.com>
2023-03-24 20:11:39 -0500

    create reaction schema

---
+++ app/thought/Reaction.js
@@ -0,0 +1,38 @@
+// import dayjs to format date
+import dayjs from 'dayjs';
+import { Schema, model } from 'mongoose';
+
+// create a schema for reactions
+const reactionSchema = new Schema(
+    {
+        reactionId: {
+            type: Schema.Types.ObjectId,
+            default: () => new Types.ObjectId()
+        },
+        reactionBody: {
+            type: String,
+            required: [true, 'Reaction body is required'],
+            maxlength: [280, 'Reaction body must be less than 280 characters long']
+        },
+        username: {
+            type: String,
+            required: [true, 'Username is required']
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now,
+            get: createdAtVal => dayjs(createdAtVal).format('MMM DD, YYYY [at] hh:mm a')
+        }
+    },
+    {
+        toJSON: { virtuals: true, getters: true },
+        id: false,
+        strict: "throw",
+        versionKey: false,
+        new: true,
+        runValidators: true
+    }
+);
+
+// create the Reaction model using the ReactionSchema
+export default reactionSchema;
