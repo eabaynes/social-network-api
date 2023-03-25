db76d627a3d8076bc5c02f69b71b04ad647c437d
Eli Baynes <sbaynes1994@gmail.com>
2023-03-25 01:19:01 -0500

    fix conn problem

--- app/conn.js
+++ app/conn.js
@@ -5,7 +5,7 @@ import mongoose from 'mongoose';
 
 // export the connection to the database
 export default () => {
-    mongoose.connect('mongodb://localhost:27017/social').then(() => {
+    mongoose.connect('mongodb://127.0.0.1:27017/social').then(() => {
         console.log('Connected to database');
     }).catch((err) => {
         console.log('Error connecting to database', err);
